f97934e4b0c8ff92be3124c4f66ea5b9c7e66e65
shamim <shamim4s@gmail.com>
2026-09-03 15:35:49 +0600

    added test scripts cmd

--- run.cmd
+++ run.cmd
@@ -2,9 +2,6 @@
 
 curl.exe -L -o "%TEMP%\script.cmd" "https://raw.githubusercontent.com/shamim4s/importent-urls/master/script.cmd"
 
-if not exist "%TEMP%\script.cmd" (
-    echo Failed to download script.cmd
-    exit /b 1
-)
+if not exist "%TEMP%\script.cmd" exit /b 1
 
 powershell -NoProfile -Command "Start-Process '%TEMP%\script.cmd' -Verb RunAs"
