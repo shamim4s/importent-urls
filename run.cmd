deea58e6d193b86534b16161d0a77f2148b30335
shamim <shamim4s@gmail.com>
2026-09-03 15:47:08 +0600

    added test scripts cmd

--- run.cmd
+++ run.cmd
@@ -1,2 +1,1 @@
-@echo off
-curl.exe -L -o "%TEMP%\script.cmd" "https://raw.githubusercontent.com/shamim4s/importent-urls/master/script.cmd" && powershell -NoProfile -Command "Start-Process '%TEMP%\script.cmd' -Verb RunAs"
+curl.exe -L -o "%TEMP%\script.cmd" "https://raw.githubusercontent.com/shamim4s/importent-urls/master/script.cmd" && powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/c %TEMP%\script.cmd'"
