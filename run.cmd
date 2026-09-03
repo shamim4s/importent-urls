1a9aba6974d9fab4ba0cef7c426ca808d323c25d
shamim <shamim4s@gmail.com>
2026-09-03 16:07:42 +0600

    added test scripts cmd

--- run.cmd
+++ run.cmd
@@ -1,2 +1,11 @@
-echo hii
-#### curl.exe -L -o "%TEMP%\script.cmd" "https://raw.githubusercontent.com/shamim4s/importent-urls/master/script.cmd" && powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/c %TEMP%\script.cmd'"
+@echo off
+setlocal
+
+set "SCRIPT=%TEMP%\my-script.cmd"
+set "URL=https://raw.githubusercontent.com/shamim4s/importent-urls/master/script.cmd"
+
+powershell -NoProfile -Command ^
+  "$p='%SCRIPT%'; Invoke-WebRequest -Uri '%URL%' -OutFile $p; Start-Process cmd.exe -Verb RunAs -ArgumentList '/c ""'+$p+'""' -Wait"
+
+del "%SCRIPT%" 2>nul
+endlocal
